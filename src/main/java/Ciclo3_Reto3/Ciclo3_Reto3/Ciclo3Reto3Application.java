bdfd8945ac34c662726976025b6969c933666956
unknown <you@example.com>
2021-10-23 12:14:34 -0500

    first commit

--- src/main/java/Ciclo3_Reto3/Ciclo3_Reto3/Ciclo3Reto3Application.java
+++ src/main/java/Ciclo3_Reto3/Ciclo3_Reto3/Ciclo3Reto3Application.java
@@ -2,8 +2,12 @@ package Ciclo3_Reto3.Ciclo3_Reto3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"Ciclo3_Reto3.Ciclo3_Reto3.model"})
 public class Ciclo3Reto3Application {
 
 	public static void main(String[] args) {
